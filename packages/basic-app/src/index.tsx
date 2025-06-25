1c0cd6a823fd792521ad604e8e9a106ced198a3c
janole <ole@mobileways.de>
2025-06-25 11:28:58 +0200

    feat: create index file to export components and types for basic-app

---
+++ packages/basic-app/src/index.tsx
@@ -0,0 +1,20 @@
+export { default as PanelApp } from './PanelApp';
+export type { AppProps } from './AppProps';
+
+export { default as FlexBox } from './Components/FlexBox';
+export { default as SplitButton } from './Components/SplitButton';
+
+export { default as ContentContainer } from './Components/ContentContainer';
+export type { ContentContainerProps } from './Components/ContentContainer';
+
+export { default as AutoGrid, Grid, GridAutoWidth, GridMaxWidth, MaxWidthContext } from './Components/AutoGrid';
+
+export { default as ResizeWatcher, useLayoutStore } from './ResizeWatcher';
+
+export { useDarkModeToggle, TernaryDarkModeProvider, TernaryDarkModeToggle } from './Components/TernaryDarkMode';
+
+export { default as TagButton } from './Components/TagButton';
+
+export { QuickMenu } from './Components/QuickMenu';
+
+export { default as ProgressBar } from './Components/ProgressBar';

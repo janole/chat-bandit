616020716dbf255a269b74fb23ff5da7d714402b
janole <ole@mobileways.de>
2025-06-25 21:48:41 +0200

    feat: add Card component for structured content display

---
+++ apps/landing-page/app/Components/Card.tsx
@@ -0,0 +1,30 @@
+import { alpha, Card as MuiCard, CardProps, useTheme } from "@mui/material";
+import BackgroundEffects from "./BackgroundEffects";
+
+export default function Card(props: CardProps)
+{
+    const { children, sx, className, ...cardProps } = props;
+
+    const theme = useTheme();
+
+    return (
+        <MuiCard
+            {...cardProps}
+            className={`card ${className}`}
+            sx={{
+                position: "relative",
+                background: alpha(theme.palette.primary.main, 0.025),
+                outline: "1px solid",
+                outlineColor: alpha(theme.palette.primary.main, 0.1),
+                pt: 2,
+                px: 2,
+                borderRadius: 3,
+                ...sx,
+            }}
+        >
+            <BackgroundEffects style={{ opacity: 0.5 }} />
+
+            {children}
+        </MuiCard>
+    );
+}

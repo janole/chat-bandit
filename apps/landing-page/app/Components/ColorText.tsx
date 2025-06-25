db457f32f98181e9edf28a993ac28468d32fa0cc
janole <ole@mobileways.de>
2025-06-25 21:48:47 +0200

    feat: add ColorText component for gradient text styling

---
+++ apps/landing-page/app/Components/ColorText.tsx
@@ -0,0 +1,34 @@
+import { Box, BoxProps, Theme } from "@mui/material";
+
+export const sxColorTextGradient = {
+    v1: {
+        backgroundImage: (theme: Theme) => `linear-gradient(to right, ${theme.palette.primary.dark}, ${theme.palette.secondary.dark})`,
+        "&:hover": {
+            backgroundImage: (theme: Theme) => `linear-gradient(to right, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
+        },
+    },
+    v2: {
+        backgroundImage: (theme: Theme) => `linear-gradient(to left, ${theme.palette.primary.dark}, ${theme.palette.secondary.dark})`,
+        "&:hover": {
+            backgroundImage: (theme: Theme) => `linear-gradient(to left, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
+        },
+    },
+    v3: {
+        backgroundImage: (theme: Theme) => `linear-gradient(to right, ${theme.palette.text.primary}, ${theme.palette.text.primary})`,
+    },
+}
+
+export default function ColorText(props: BoxProps & { variant: keyof typeof sxColorTextGradient })
+{
+    return (
+        <Box
+            component="span"
+            {...props}
+            sx={{
+                ...sxColorTextGradient[props.variant ?? "v1"],
+                backgroundClip: "text",
+                color: "transparent",
+            }}
+        />
+    );
+}

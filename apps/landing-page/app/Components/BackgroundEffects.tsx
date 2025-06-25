d77378aa11a17c87051a4420e063475ad062c290
janole <ole@mobileways.de>
2025-06-25 21:48:24 +0200

    feat: add BackgroundEffects component for decorative background elements

---
+++ apps/landing-page/app/Components/BackgroundEffects.tsx
@@ -0,0 +1,12 @@
+import { Box, BoxProps } from "@mui/material";
+
+export default function BackgroundEffects(props: BoxProps)
+{
+    return (
+        <Box {...props} position="absolute" left={0} top={0} right={0} bottom={0}>
+            <div className="absolute top-1/4 left-1/4 w-64 h-64 bg-purple-500/10 rounded-full blur-3xl"></div>
+            <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl"></div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-gradient-to-r from-purple-500/5 to-blue-500/5 rounded-full blur-3xl"></div>
+        </Box>
+    );
+}

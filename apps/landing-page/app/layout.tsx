e08a11945c1f579eb2b4285bff049d24edad2720
janole <ole@mobileways.de>
2025-06-26 22:06:47 +0200

    feat: enhance metadata in layout.tsx with additional SEO properties

--- apps/landing-page/app/layout.tsx
+++ apps/landing-page/app/layout.tsx
@@ -3,8 +3,22 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+    authors: [{ name: "Jan Ole Suhr", url: "https://janole.com" }],
+    creator: "Jan Ole Suhr",
     title: "Chat Bandit",
     description: "Chat with Local and Cloud AI. The ultimate desktop AI companion. Run local models with Ollama & llama.cpp, or connect to OpenAI, OpenRouter, Google AI and many more. Privacy-focused, lightning-fast, and infinitely customizable.",
+    applicationName: "Chat Bandit Landing Page",
+    keywords: ["Chat Bandit", "AI", "Chatbot", "LLM", "Ollama", "llama.cpp"],
+    icons: {
+        icon: "/public/icon.png",
+    },
+    twitter: {
+        card: "summary_large_image",
+        images: "/public/screenshot-light.png",
+        site: "@mobileways",
+        creator: "@janole",
+    },
+    robots: "index, follow",
 };
 
 export default function RootLayout({

d6052f125533db87f9d49634391debc773f0cd03
janole <ole@mobileways.de>
2025-06-26 22:07:09 +0200

    feat: update HeroHeadline component to support responsive font sizes and line heights

--- apps/landing-page/app/Components/HeroHeadline.tsx
+++ apps/landing-page/app/Components/HeroHeadline.tsx
@@ -5,10 +5,10 @@ export default function HeroHeadline(props: BoxProps & { size?: "xl" | "md" | "s
     const { size, ...boxProps } = props;
 
     const params = {
-        xl: { fontSize: "4.50rem", fontWeight: 800, lineHeight: 1.0, color: "text.primary", pb: 3, sx: { textShadow: "1px 1px 2px #0001" } },
-        md: { fontSize: "3.00rem", fontWeight: 700, lineHeight: 1.1, color: "text.primary" },
-        sm: { fontSize: "1.25rem", fontWeight: 400, lineHeight: 1.75, color: "text.secondary", maxWidth: { lg: "50%", md: "75%", xs: "100%" } },
-        xs: { fontSize: "0.85rem", fontWeight: 400, lineHeight: 1.75, color: "text.secondary", maxWidth: { lg: "50%", md: "75%", xs: "100%" } },
+        xl: { fontSize: { md: "4.50rem", sm: "2.5rem", xs: "2.5rem" }, fontWeight: 800, lineHeight: { md: 1.0, sm: 1.1, xs: 1.1 }, color: "text.primary", sx: { textShadow: "1px 1px 2px #0001" } },
+        md: { fontSize: { md: "3.00rem", sm: "1.5rem", xs: "1.5rem" }, fontWeight: 700, lineHeight: 1.1, color: "text.primary" },
+        sm: { fontSize: { md: "1.25rem" }, fontWeight: 400, lineHeight: 1.75, color: "text.secondary", maxWidth: { lg: "50%", md: "75%", xs: "100%" } },
+        xs: { fontSize: { md: "0.85rem" }, fontWeight: 400, lineHeight: 1.75, color: "text.secondary", maxWidth: { lg: "50%", md: "75%", xs: "100%" } },
     }
 
     return (

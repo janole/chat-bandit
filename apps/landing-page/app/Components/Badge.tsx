c124190c1747c7923fc0e94ce4f6f1206c9f6a42
janole <ole@mobileways.de>
2025-06-26 22:07:03 +0200

    feat: enhance Badge component to support icon rendering and wrap in WobbleGrid

--- apps/landing-page/app/Components/Badge.tsx
+++ apps/landing-page/app/Components/Badge.tsx
@@ -1,20 +1,25 @@
-import { alpha, Chip, ChipProps, Theme } from "@mui/material";
+import { alpha, Box, Chip, ChipProps, Theme } from "@mui/material";
+import { WobbleGrid } from "./Feature";
 
 export default function Badge(props: ChipProps)
 {
-    const { ...chipProps } = props;
+    const { icon, ...chipProps } = props;
 
     return (
-        <Chip
-            {...chipProps}
-            sx={{
-                p: 2,
-                color: "text.secondary",
-                background: (theme: Theme) => alpha(theme.palette.primary.main, 0.05),
-                border: "1px solid",
-                borderColor: (theme: Theme) => alpha(theme.palette.primary.main, 0.1),
-                boxShadow: 1,
-            }}
-        />
+        <WobbleGrid>
+            <Chip
+                {...chipProps}
+                icon={icon && <Box className="icon">{icon}</Box>}
+                sx={{
+                    p: { xs: 0.5, sm: 1, md: 2 },
+                    fontWeight: 500,
+                    color: "text.secondary",
+                    background: (theme: Theme) => alpha(theme.palette.primary.main, 0.05),
+                    border: "1px solid",
+                    borderColor: (theme: Theme) => alpha(theme.palette.primary.main, 0.1),
+                    boxShadow: 1,
+                }}
+            />
+        </WobbleGrid>
     );
 }

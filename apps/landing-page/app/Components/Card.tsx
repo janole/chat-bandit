6c9dff1a5afe902eb9574e3ee3ab38141b9a1f15
janole <ole@mobileways.de>
2025-06-27 16:58:51 +0200

    fix: ensure overflow is hidden in Card component for better layout control

--- apps/landing-page/app/Components/Card.tsx
+++ apps/landing-page/app/Components/Card.tsx
@@ -19,6 +19,7 @@ export default function Card(props: CardProps)
                 pt: 2,
                 px: 2,
                 borderRadius: 3,
+                overflow: "hidden",
                 ...sx,
             }}
         >

07e070915b0c4e5074756db6e3bb5a0c2fa02d53
janole <ole@mobileways.de>
2025-06-30 17:13:47 +0200

    fix: standardize import statements and add tsup configuration file

--- packages/basic-app/src/index.tsx
+++ packages/basic-app/src/index.tsx
@@ -1,10 +1,10 @@
-export type { AppProps } from './AppProps';
+export type { AppProps } from "./AppProps";
 export { default as BasicApp } from './App';
 export { default as FullscreenApp } from './FullscreenApp';
 export { default as PanelApp } from './PanelApp';
 
-export { default as FlexBox } from './Components/FlexBox';
-export { default as SplitButton } from './Components/SplitButton';
+export { default as FlexBox, Spacer } from './Components/FlexBox';
+export { default as SplitButton, NavBar } from './Components/SplitButton';
 
 export { default as ContentContainer } from './Components/ContentContainer';
 export type { ContentContainerProps } from './Components/ContentContainer';
@@ -17,6 +17,8 @@ export { useDarkModeToggle, TernaryDarkModeProvider, TernaryDarkModeToggle } fro
 
 export { default as TagButton } from './Components/TagButton';
 
-export { QuickMenu } from './Components/QuickMenu';
+export { QuickMenu } from "./Components/QuickMenu";
 
-export { default as ProgressBar } from './Components/ProgressBar';
+export { default as ProgressBar } from "./Components/ProgressBar";
+
+export { default as SpinningButton, CancelButton } from "./Components/SpinningButton";

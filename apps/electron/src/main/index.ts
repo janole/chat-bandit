3cdefa810fe6f591c226412c72806936ea7ffc32
janole <ole@mobileways.de>
2025-07-02 21:10:35 +0200

    fix: update import path for registerAdapter in index.ts

--- apps/electron/src/main/index.ts
+++ apps/electron/src/main/index.ts
@@ -2,7 +2,7 @@ import { app, BrowserWindow, Menu, ipcMain } from "electron";
 import { electronApp, optimizer } from "@electron-toolkit/utils";
 import electronUpdater from "electron-updater";
 import { tryCatchCache } from "@janole/try-catch";
-import { registerAdapter } from "../libraries/ai/ElectronServer";
+import { registerAdapter } from "@janole/ai-electron/electron";
 import { registerStore } from "../libraries/electron-store/server";
 import { createMainWindow, registerWindowManager, send } from "./WindowManager";
 import { createMenu } from "./MenuManager";

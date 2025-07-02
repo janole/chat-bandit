c9d0ff519ec02dd6915d303086edfd12cda27c27
janole <ole@mobileways.de>
2025-07-02 11:48:43 +0200

    style: standardize import quotes in main.tsx

--- apps/electron/src/renderer/src/main.tsx
+++ apps/electron/src/renderer/src/main.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import "./main.css";
 

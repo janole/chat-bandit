df0b2a8a4fb5aa3d66e06019a637d88a4cf381e8
janole <ole@mobileways.de>
2025-07-02 13:33:31 +0200

    feat: add ModelManager component with theme options and context

---
+++ apps/electron/src/renderer/src/Chat/ModelManager.tsx
@@ -0,0 +1,31 @@
+import { ThemeOptions } from "@mui/material";
+import { ModelManager as AiModelManager } from "@janole/ai-chat";
+import { withAppContext } from "@renderer/AppContext";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
+
+const themeOptions: ThemeOptions = {
+    typography: {
+        fontFamily: "Roboto",
+    },
+};
+
+function ModelManager()
+{
+    return (
+        <AiModelManager
+            themeOptions={themeOptions}
+            toolbarLeftOffset="80px"
+        />
+    );
+}
+
+const Component = withAppContext(ModelManager);
+
+export default Component;
+
+export { Component };

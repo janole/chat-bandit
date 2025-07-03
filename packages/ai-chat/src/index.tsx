1cfb4fc29fda7a71b02f2382e4ef8991bb841d46
janole <ole@mobileways.de>
2025-07-03 20:45:43 +0200

    fix: remove duplicate export of Navigation component in index.tsx

--- packages/ai-chat/src/index.tsx
+++ packages/ai-chat/src/index.tsx
@@ -1,9 +1,8 @@
 export * from "@janole/ai-core";
 
 export { default as Chat } from "./Chat";
+export * from "./Navigation";
 export { default as ModelManager } from "./ModelManager";
 
 export * from "./Components/Markdown";
 export * from "./Components/DownloadableModelCard";
-
-export * from "./Navigation";
